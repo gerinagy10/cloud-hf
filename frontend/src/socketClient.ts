df0557a1d355d6727a51781ec20765b56496f780
gerinagy <100428879+gerinagy10@users.noreply.github.com>
2025-05-06 14:54:40 +0300

    Fix urls

--- frontend/src/socketClient.ts
+++ frontend/src/socketClient.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://cloudhf-notifier:8000", {
+export const socket = io("http://localhost:80084", {
   transports: ["websocket"],
 });
