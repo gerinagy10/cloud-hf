6539472ec05402867e6fc3b4135417c4bae2da3f
gerinagy <100428879+gerinagy10@users.noreply.github.com>
2025-05-06 16:24:47 +0300

    fix url

--- frontend/src/socketClient.ts
+++ frontend/src/socketClient.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:80084", {
+export const socket = io("http://localhost:30084", {
   transports: ["websocket"],
 });
